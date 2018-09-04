791b96d967f03cfb89bfedc1bf1bdef66df81e48
Md Jahid Raihan <jahid.raihan10@gmail.com>
2018-09-04 22:52:10 +0600

    Update articulation_point.cpp

--- articulation_point.cpp
+++ articulation_point.cpp
@@ -1,90 +1,103 @@
-#include<bits/stdc++.h>
+#include<algorithm>
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
+#include<cmath>
 using namespace std;
-
-
+ 
+#define mx 10009
 #define nil -1
 #define inf 10000
-#define mx 1000
 #define white 0
 #define black 2
 #define grey 1
 vector<int>edge[mx],cost[mx];
-int color[mx],low[mx],f[mx],d[mx];
+int color[mx],low[mx],f[mx],d[mx],num_of_children[mx];
 int parent[mx+1];
-int time;
-int e,n;
+int tme;
 bool arti[mx];
-
+int e,n;
+int cnt;
 void init()
 {
-	for(int i=0;i<n;i++)
-	{
-		color[i]=white;
-		parent[i] = nil;
-		low[i]=inf;
-		f[i]=inf;
-		d[i]=inf;
-
-	}
-
+    for(int i=0;i<=n;i++)
+    {
+        color[i]=white;
+        parent[i] = nil;
+        low[i]=inf;
+        f[i]=inf;
+        d[i]=inf;
+    }
+ 
 }
-void dfs(int p)
+void dfs(int u)
 {
-	time++;int children = 0;
-	color[p]=grey;
-	d[p] =time;
-	for(int i=0;i<edge[p].size();i++)
-	{
-		int v =edge[p][i];
-		if(color[v]==white)
-		{
-			children++;
-
-			parent[v] = p;
-			dfs(v);
-			low[p] = min(low[v],low[p]);
-			if(children>1 && parent[p]==nil)
-				arti[p]=true;
-			if(parent[p]!=nil && low[v]>=d[p])
-				arti[p] =true;
-
-		}
-		else if(v != parent[p])
-			low[p]=min(low[p],d[v]);
-	}
-	color[p]=black;
-	time++;
-	f[p] =time;
-
-
+    tme++;int children = 0;
+    color[u]=grey;
+    d[u] =tme;
+    for(int i=0;i<edge[u].size();i++)
+    {
+        int v =edge[u][i];
+        if(color[v]==white)
+        {
+            children++;
+            parent[v] = u;
+            dfs(v);
+            low[u] = min(low[u],low[v]);
+ 
+            if(low[v]>=d[u])
+                arti[u] =true;
+ 
+        }
+        else if(v != parent[u])
+            low[u]=min(low[u],d[v]);
+    }
+    num_of_children[u]=children;
+    color[u] = black;
+    //tme++;
+    f[u]=tme;
 }
 int  main()
 {
-
-	cin>>n>>e;
-	for(int i=0;i<e;i++)
-	{
-		int x,y;
-		cin>>x>>y;
-		edge[x].push_back(y);
-		edge[y].push_back(x);
-
-	}
-	init();
-	time=0;
-	for(int i=0;i<n;i++)
-	{
-		if(color[i]==white)
-		{
-			dfs(i);
-		}
-
-	}
-	cout<<"articulation point"<<endl;
-	for(int i=0;i<n;i++)
-	{
-		if(arti[i])
-			cout<<i<<" ";
-	}
-	return 0;
+ 
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int T;
+    scanf("%d",&T);
+    for(int cases=1;cases<=T;cases++)
+    {
+        fill_n(num_of_children,10005,0);
+        cin>>n>>e;
+        for(int i=0;i<e;i++)
+        {
+            int x,y;
+            cin>>x>>y;
+            edge[x].push_back(y);
+            edge[y].push_back(x);
+ 
+        }
+        init();
+        tme=0;
+        for(int i=0;i<mx;i++) arti[i] =false;
+        //memset(arti,false,sizeof(arti));
+        cnt=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(color[i]==white)
+            {
+                dfs(i);
+                arti[i]=(num_of_children[i]>1);
+            }
+ 
+        }
+        for(int i=1;i<=n;i++)
+            if(arti[i]==true) cnt++;
+        printf("Case %d: %d\n",cases,cnt);
+        for(int i=0;i<mx;i++)
+            edge[i].clear();
+    }
+    return 0;
 }
